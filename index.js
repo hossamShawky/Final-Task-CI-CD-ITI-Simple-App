0d82f0cab36b4dd13c79ddd36d086dc6628dc74e
hossam_shawky <hossamshawky288@gmail.com>
2023-06-10 23:15:34 +0300

    simple app

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-function alertDate() {
-    var currentDate = new Date();
-    alert("Today's date is: " + currentDate.toDateString());
-  }
-  
-  function alertTime() {
-    var currentTime = new Date();
-    alert("The current time is: " + currentTime.toLocaleTimeString());
-  }
+var dateBtn = document.getElementById("dateBtn");
+var timeBtn = document.getElementById("timeBtn");
+var display = document.getElementById("display");
+
+dateBtn.addEventListener("click", function() {
+  var currentDate = new Date();
+  display.innerHTML = "Today's date is: " + currentDate.toDateString();
+});
+
+timeBtn.addEventListener("click", function() {
+  var currentTime = new Date();
+  display.innerHTML = "The current time is: " + currentTime.toLocaleTimeString();
+});
